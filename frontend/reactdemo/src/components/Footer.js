b674eaa588eb8c56f871c1e153cc871136dc0dae
zaidaliyan <157130369+zaidaliyan@users.noreply.github.com>
2024-05-28 11:37:02 +0530

    initial commit

---
+++ frontend/reactdemo/src/components/Footer.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-light py-4 mt-auto">
+      <div className="container text-center">
+        <p>&copy; 2024 HelpDesk Pro. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
