b2997287ec414351c9104cc735bd62732feee073
Pranav <158553544+cdpranavkethan@users.noreply.github.com>
2024-05-30 16:18:21 +0530

    Delete backend/backend directory

--- backend/backend/models/agent.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const agentSchema=new mongoose.Schema({
-    name:String,
-    ticketsResolved:Number,
-    customerSatisfaction:Number
-});
-
-module.exports = mongoose.model('agentSchema',agentSchema)
